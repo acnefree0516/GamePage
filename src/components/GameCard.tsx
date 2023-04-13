1f77003476f9435a99f88f07b0ef0fa15b9e0884
Jia <tangjia0516@gmail.com>
2023-04-13 19:25:22 -0400

    Display scores

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import Score from "./Score";
 
 interface Props {
   game: Game;
@@ -12,10 +13,13 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
-        {/* extract platform from game.parent_platforms*/}
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          {/* extract platform from game.parent_platforms*/}
+          <Score score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
